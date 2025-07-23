49552022761f494fb9a2381772b4aaa39ecfbc55
Alejandro Rosales <arosales300399@gmail.com>
2025-07-22 22:17:10 -0600

    View SobreNosotros Completada

---
+++ views/Contactanos&SobreNosotros/js/sobreNosotros.js
@@ -0,0 +1,9 @@
+// cambia el fondo de la pagina
+function changeDark() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+function changeWhite() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
